2f0dbfe578fb1de701487b520948f2e1ced1f0a4
Aziat <abdimalikovaziat@gmail.com>
2022-11-15 16:28:24 +0600

    HomeWork-19 v2

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,52 +1,67 @@
 import { useState, useRef } from "react";
 
 const BasicForm = (props) => {
-  const nameInputRef = useRef();
-  const [nameIsValid, setNameIsValid] = useState(true);
+  const firstNameInputRef = useRef();
+  const lastNameInputRef = useRef();
+  const [firstNameIsValid, setFirstNameIsValid] = useState(true);
+  const [lastNameIsValid, setLastNameIsValid] = useState(true);
   const [name, setName] = useState();
-  const [isTouched, setIsTouched] = useState()
-
+  const [isTouched, setIsTouched] = useState();
 
   const onChangeHandler = (e) => {
     setName(e.target.value);
+    setFirstNameIsValid(true);
+    setLastNameIsValid(true);
+    setIsTouched(true);
 
-    setNameIsValid(true);
-    setIsTouched(true)
-
-    const enteredName = nameInputRef.current.value;
-
-  if(enteredName.trim().length < 4 ) {
-      setNameIsValid(false)
-      setIsTouched(false)
-      return
+    const firstEnteredName = firstNameInputRef.current.value;
+    const lastEnteredName = lastNameInputRef.current.value;
+ 
+    if (firstEnteredName.trim().length < 4) {
+      setFirstNameIsValid(false);
+      setIsTouched(false);
+      return;
+    } else if (lastEnteredName.trim().length < 6) {
+      setLastNameIsValid(false);
+      setIsTouched(false);
     }
   };
-  // const validateEmail = (email) => {
-  //   return String(email)
-  //     .toLowerCase()
-  //     .match(
-  //       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  //     );
-  // };
 
-  const nameInputClasses = nameIsValid ? "form-control" : "form-control invalid"
-
+  const nameInputClasses = firstNameIsValid
+    ? "form-control"
+    : "form-control invalid";
 
   return (
     <form>
       <div className={nameInputClasses}>
         <div className="form-control">
           <label htmlFor="name">First Name</label>
-          <input type="text" id="name" ref={nameInputRef} onChange={onChangeHandler} />
+          <input
+            type="text"
+            id="name"
+            ref={firstNameInputRef}
+            onChange={onChangeHandler}
+          />
+          {!firstNameIsValid && (
+            <p className="error-text">name should be more 4 characters</p>
+          )}
         </div>
         <div className="form-control">
           <label htmlFor="name">Last Name</label>
-          <input type="text" id="name" ref={nameInputRef} onChange={onChangeHandler} />
+          <input
+            type="text"
+            id="name"
+            ref={lastNameInputRef}
+            onChange={onChangeHandler}
+          />
+          {!lastNameIsValid && (
+            <p className="error-text">name should be more 6 characters</p>
+          )}
         </div>
       </div>
       <div className="form-control">
         <label htmlFor="email">E-Mail Address</label>
-        <input type="email" id="email"/>
+        <input type="email" id="email" required />
       </div>
       <div className="form-actions">
         <button disabled={!isTouched}>Submit</button>
